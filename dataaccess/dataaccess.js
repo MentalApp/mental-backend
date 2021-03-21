d80a24eb5a25880bce87b3d54163be6d15ab7dcb
namdongngac <ntnam7399@gmail.com>
2021-03-21 13:57:29 +0700

    Database connection

--- dataaccess/dataaccess.js
+++ dataaccess/dataaccess.js
@@ -1,3 +1,32 @@
 /**
  * Access database in here
  */
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+  host: 'localhost',                         // change host here
+  database: 'mental_development',
+  user: 'root', 
+  password: '',
+  socketPath: '/var/run/mysqld/mysqld.sock', // change socket
+  connectionLimit: 5
+});
+pool.getConnection()
+  .then(conn => {
+    conn.query("SELECT 1 as val")
+      .then((rows) => {
+        console.log(rows);
+        return "Connected"
+      })
+      .then((res) => {
+        console.log(res);
+        conn.end();
+      })
+      .catch(err => {
+        //handle error
+        console.log(err);
+        conn.end();
+      })
+  }).catch(err => {
+    //not connected
+    console.log(err);
+  });
