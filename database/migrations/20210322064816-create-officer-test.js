d8b8afa6d5c6a635ee3d85fe6bd5656d095b865b
namdongngac <ntnam7399@gmail.com>
2021-03-22 14:24:43 +0700

    Create models

---
+++ database/migrations/20210322064816-create-officer-test.js
@@ -0,0 +1,69 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('OfficerTests', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      question: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      dateOfBirth: {
+        type: Sequelize.DATE
+      },
+      gender: {
+        type: Sequelize.INTEGER
+      },
+      nation: {
+        type: Sequelize.STRING
+      },
+      joinArmy: {
+        type: Sequelize.DATE
+      },
+      unit: {
+        type: Sequelize.STRING
+      },
+      rank: {
+        type: Sequelize.STRING
+      },
+      position: {
+        type: Sequelize.STRING
+      },
+      answer: {
+        type: Sequelize.TEXT
+      },
+      isLieConfirm: {
+        type: Sequelize.BOOLEAN
+      },
+      liePredictPercent: {
+        type: Sequelize.DOUBLE
+      },
+      testTime: {
+        type: Sequelize.INTEGER
+      },
+      testVersion: {
+        type: Sequelize.STRING
+      },
+      userConfirmId: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('OfficerTests');
+  }
+};
---
+++ database/seeders/seeds.js
@@ -0,0 +1,1 @@
+// Write seed here
