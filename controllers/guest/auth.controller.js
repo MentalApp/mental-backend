859974676fe883540bf3b9dd1adc25888cbb6c95
Nguyen Thanh Nam <ntnam7399@gmail.com>
2021-03-24 16:10:36 +0700

    Improve Auth service

---
+++ controllers/guest/auth.controller.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const appSetting = require('../../appconfig/app.config');
+const db = require("../../database/models");
+const Test = db.Test;
+
+const authController = {
+  joinin: async (req, res) => {
+    test = await Test.findOne({ where: { code: req.body.code } })
+    console.log(test)
+    if (!test) {
+      res.status(404).json({ message: 'The test not found' });
+    } else if (test) {
+      token = jwt.sign({ 
+        code: test.code,
+        testVersion: test.testVersionId,
+        id: test.id,
+        role: "guest"
+      }, appSetting.jwtConfig.secretKey)
+
+      return res.json({ token: token });
+    }
+  }
+}
+
+module.exports = authController;
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,12 +4,28 @@ const { StatusCodes, getReasonPhrase } = require('http-status-codes');
 
 const authMiddleware = {
   authGuest: async (req, res, next) => {
-    next();
+    if (req.headers[appSetting.authKey]) {
+      const token = req.headers[appSetting.authKey];
+      const decode = jwt.verify(token, appSetting.jwtConfig.secretKey)
+      console.log(decode)
+      if (!!decode && decode.role == "guest") {
+        next();
+      } else {
+        res.status(StatusCodes.UNAUTHORIZED).json({
+          message: getReasonPhrase(StatusCodes.UNAUTHORIZED)
+        })
+      }
+    } else {
+      res.status(StatusCodes.UNAUTHORIZED).json({
+        message: getReasonPhrase(StatusCodes.UNAUTHORIZED)
+      })
+    }
   },
   authAdmin: async (req, res, next) => {
     if (req.headers[appSetting.authKey]) {
       const token = req.headers[appSetting.authKey];
-      if (jwt.verify(token, appSetting.jwtConfig.secretKey)) {
+      const decode = jwt.verify(token, appSetting.jwtConfig.secretKey)
+      if (!!decode && decode.role == "admin") {
         next();
       } else {
         res.status(StatusCodes.UNAUTHORIZED).json({
