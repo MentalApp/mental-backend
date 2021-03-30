194e39f667d5082207e8dfc241ba2e3523ac2642
Nguyen Thanh Nam <56693949+NamNguyen99@users.noreply.github.com>
2021-03-30 17:07:33 +0700

    config heroku (#14)

--- database/seeders/seeds.js
+++
@@ -1,1 +0,0 @@
-// Write seed here
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const env = require('dotenv');
+const cors = require('cors')
 env.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res) => {
