8786e443198a0a3a41b254005821771ffb43d756
vu dang <vu.dang@sotatek.com>
2021-04-19 18:04:25 +0700

    update api user

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -1,51 +1,69 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require("bcrypt-nodejs");
+const bcrypt = require('bcrypt-nodejs');
 
 const resultUtil = require('../../servicehelper/service.result');
 const exceptionUtil = require('../../handler_error/exceptionUtil');
 
 const appSetting = require('../../appconfig/app.config');
-const db = require("../../database/models");
+const db = require('../../database/models');
+const {ErrorMessage} = require('../../helpers/constant');
 const User = db.User;
 
 const authController = {
   login: async (req, res) => {
-    const serviceResult = resultUtil.new();
-    try {
-      user = await User.findOne({ where: { email: req.body.email } })
-      if (!user) {
-        serviceResult.success = false;
-        serviceResult.error = 'Authentication failed. User not found.';
-        serviceResult.code = 404;
-      } else {
-        if (!bcrypt.compareSync(req.body.password, user.password)) {
-          serviceResult.success = false;
-          serviceResult.error = 'Authentication failed. Wrong password.';
-          serviceResult.code = 401;
-        } else {
-          token = jwt.sign({
-            email: user.email,
-            name: user.fullName,
-            id: user.id,
-            role: "admin"
-          }, appSetting.jwtConfig.secretKey, {
-            expiresIn: appSetting.jwtConfig.expire
-          })
-          serviceResult.code = 200;
-          serviceResult.success = true;
-          serviceResult.token = token;
-        }
-      }
-    } catch (error) {
-      exceptionUtil.handlerErrorAPI(res, serviceResult, error);
-    } finally {
-      res.json(serviceResult);
+    if (!req.body.email || !req.body.password) {
+      res.status(400).json({
+        success: false,
+        error: ErrorMessage.USERNAME_AND_PASSWORD_IS_REQUIRE,
+      });
     }
+    const user = await User.findOne({where: {email: req.body.email}});
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        error: ErrorMessage.USER_NOT_FOUND,
+      });
+    }
+
+    if (user.isBlock === 1) {
+      res.status(404).json({
+        success: false,
+        code: 1001,
+        error: ErrorMessage.USER_IS_BLOCKING,
+      });
+    }
+
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      res.status(404).json({
+        success: false,
+        error: ErrorMessage.WRONG_PASSWORD,
+      });
+    }
+    token = jwt.sign(
+      {
+        email: user.email,
+        name: user.fullName,
+        id: user.id,
+        role: user.role,
+      },
+      appSetting.jwtConfig.secretKey,
+      {
+        expiresIn: appSetting.jwtConfig.expire,
+      },
+    );
+
+    res.status(200).json({
+      success: true,
+      data: {
+        token: token,
+        id: user.id,
+        role: user.role,
+      },
+    });
   },
 
   logout: async (req, res) => {
     try {
-
     } catch (error) {
       res.status(400).message(error);
     }
@@ -56,23 +74,23 @@ const authController = {
     try {
       const decode = jwt.verify(token, appSetting.jwtConfig.secretKey);
       if (decode) {
-        const user = await User.findOne({ where: { email: decode.email } });
+        const user = await User.findOne({where: {email: decode.email}});
         res.status(200).json({
-          message: user
+          message: user,
         });
       } else {
         res.status(200).json({
           success: false,
-          error: "Token is invalid"
+          error: ErrorMessage.TOKEN_IS_INVALID,
         });
       }
     } catch (error) {
       res.status(400).json({
         success: false,
-        error: error.message
+        error: error.message,
       });
     }
-  }
-}
+  },
+};
 
 module.exports = authController;
--- controllers/admin/user.controller.js
+++ controllers/admin/user.controller.js
@@ -1,115 +1,286 @@
-const db = require("../../database/models");
-const userSerializer = require("../../serializers/user.serializer")
+const db = require('../../database/models');
+const userSerializer = require('../../serializers/user.serializer');
+const {Roles, ErrorMessage} = require('../../helpers/constant');
+const {removeVietnameseTones} = require('../../helpers/vietnameseTone.helper');
+const {DefaultPaging} = require('../../helpers/constant');
+const bcrypt = require('bcrypt-nodejs');
 
 const User = db.User;
 const Op = db.Sequelize.Op;
 
-const userController = {
+const UserController = {
   create: async (req, res) => {
-    const user = req.body;
-
-    User.create(user)
-      .then(data => {
-        res.json({
-          success: true,
-          data: userSerializer.new(data)
-        });
-      })
-      .catch(err => {
-        res.json({
-          success: false,
-          error: err.message || "Some error occurred while creating the User."
-        });
+    if (!req.body.email || !req.body.password || !req.body.fullName || !req.body.militaryCode) {
+      res.status(400).json({
+        success: false,
+        error: ErrorMessage.INFORMATION_USER_IS_NOT_ENOUGH,
       });
+    }
+
+    const isExist = await User.findOne({where: {email: req.body.email}});
+    const isExistMilitaryCode = await User.findOne({where: {militaryCode: req.body.militaryCode}});
+
+    if (isExist) {
+      res.status(400).json({
+        success: false,
+        error: ErrorMessage.USER_IS_EXISTED,
+      });
+    }
+
+    if (isExistMilitaryCode) {
+      res.status(400).json({
+        success: false,
+        error: ErrorMessage.MILITARYCODE_IS_EXISTED,
+      });
+    }
+
+    const user = {
+      email: req.body.email,
+      password: req.body.password,
+      fullName: req.body.fullName,
+      militaryCode: req.body.militaryCode,
+      isBlock: 2,
+      nameWithoutUtf8: removeVietnameseTones(req.body.fullName),
+      ...(req.body.role && {role: req.body.role}),
+    };
+
+    const save = await User.create(user);
+    if (!save) {
+      res.status(404).json({
+        success: false,
+        error: ErrorMessage.CREATE_USER_IS_NOT_SUCCESS,
+      });
+    }
+
+    res.status(201).json({
+      success: true,
+      data: userSerializer.new(save),
+    });
   },
 
   findAll: async (req, res) => {
     const title = req.query.title;
-    var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+    var condition = title ? {title: {[Op.like]: `%${title}%`}} : null;
 
-    User.findAll({ where: condition })
-      .then(data => {
+    User.findAll({where: condition})
+      .then((data) => {
         res.json({
           success: true,
-          data: data.map(item => userSerializer.new(item))
+          data: data.map((item) => userSerializer.new(item)),
         });
       })
-      .catch(err => {
+      .catch((err) => {
         res.json({
           success: false,
-          error: err.message || "Some error occurred while retrieving users."
+          error: err.message || 'Some error occurred while retrieving users.',
         });
       });
+  },
+
+  indexDoctors: async (req, res) => {
+    const perPage = +req.query.perPage || DefaultPaging.perPage;
+    const page = +req.query.page || DefaultPaging.page;
+
+    const condition = {
+      role: 'doctor',
+    };
+
+    const {count, rows} = await User.findAndCountAll({
+      where: condition,
+      order: [['id', 'DESC']],
+      limit: perPage,
+      offset: perPage * page - perPage,
+    });
+    if (!rows) {
+      res.status(404).json({
+        success: false,
+        error: ErrorMessage.USERS_IS_NOT_FOUND,
+      });
+    }
+    res.json({
+      success: true,
+      totalPages: Math.ceil(count / perPage),
+      data: rows.map((item) => userSerializer.new(item)),
+    });
   },
 
   findOne: async (req, res) => {
-    const id = req.params.id;
-
-    User.findByPk(id)
-      .then(data => {
-        res.json({
-          success: true,
-          data: userSerializer.new(data)
-        });
-      })
-      .catch(err => {
-        res.status(404).json({
-          success: false,
-          error: err.message || "Error retrieving User with id=" + id
-        });
+    if (!req.params.id) {
+      res.status(400).json({
+        success: false,
+        error: ErrorMessage.ID_IS_INVALID,
       });
+    }
+    const find = await User.findByPk(req.params.id);
+
+    if (!find) {
+      res.status(404).json({
+        success: false,
+        error: 'Error retrieving User with id=' + req.params.id,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: userSerializer.new(find),
+    });
   },
 
   update: async (req, res) => {
-    const id = req.params.id;
-    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(8), null)
+    if (!req.params.id) {
+      res.status(400).json({
+        success: false,
+        error: ErrorMessage.ID_IS_INVALID,
+      });
+    }
 
-    User.update(req.body, {
-      where: { id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.json({
-            success: true
-          });
-        } else {
-          res.json({
-            success: false
-          });
-        }
-      })
-      .catch(err => {
+    const user = await User.findOne({where: {id: req.params.id}});
+    console.log(user);
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        error: ErrorMessage.USER_NOT_FOUND,
+      });
+    }
+
+    if (req.body.isBlock) {
+      res.status(401).json({
+        success: false,
+        error: ErrorMessage.ACCOUNT_IS_UNAUTHORIZE,
+      });
+    }
+
+    if (req.body.password && !req.body.newPassword) {
+      res.status(400).json({
+        success: false,
+        error: ErrorMessage.BAD_REQUEST,
+      });
+    }
+
+    if (req.body.password && req.body.newPassword) {
+      if (!bcrypt.compareSync(req.body.password, user.password)) {
         res.status(400).json({
           success: false,
-          error: err.message || "Error updating User with id=" + id
+          error: ErrorMessage.BAD_REQUEST,
         });
+      }
+      req.body.password = bcrypt.hashSync(req.body.newPassword, bcrypt.genSaltSync(8), null);
+    }
+    const save = await User.update(
+      {
+        ...req.body,
+        ...(req.body.fullName && {nameWithoutUtf8: removeVietnameseTones(req.body.fullName)}),
+      },
+      {
+        where: {id: req.params.id},
+      },
+    );
+    if (!save) {
+      res.status(404).json({
+        success: false,
+        error: ErrorMessage.UPDATE_IS_FAILED,
       });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: save,
+    });
+  },
+
+  updateWithRoleAdmin: async (req, res) => {
+    if (!req.params.id) {
+      res.status(400).json({
+        success: false,
+        error: ErrorMessage.ID_IS_INVALID,
+      });
+    }
+
+    const user = await User.findOne({where: {id: req.params.id}});
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        error: ErrorMessage.USER_NOT_FOUND,
+      });
+    }
+
+    if (req.body.password && !req.body.newPassword) {
+      res.status(400).json({
+        success: false,
+        error: ErrorMessage.BAD_REQUEST,
+      });
+    }
+
+    if (req.body.password && req.body.newPassword) {
+      if (!bcrypt.compareSync(req.body.password, user.password)) {
+        res.status(400).json({
+          success: false,
+          error: ErrorMessage.BAD_REQUEST,
+        });
+      }
+      req.body.password = bcrypt.hashSync(req.body.newPassword, bcrypt.genSaltSync(8), null);
+    }
+
+    const save = await User.update(
+      {
+        ...req.body,
+        ...(req.body.fullName && {nameWithoutUtf8: removeVietnameseTones(req.body.fullName)}),
+      },
+      {
+        where: {id: req.params.id},
+      },
+    );
+
+    if (!save) {
+      res.status(404).json({
+        success: false,
+        error: ErrorMessage.UPDATE_IS_FAILED,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: save,
+    });
   },
 
   delete: async (req, res) => {
-    const id = req.params.id;
+    if (!req.params.id) {
+      res.status(400).json({
+        success: false,
+        error: ErrorMessage.ID_IS_INVALID,
+      });
+    }
+
+    const user = await User.findOne({where: {id: req.params.id}});
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        error: ErrorMessage.USER_NOT_FOUND,
+      });
+    }
 
     User.destroy({
-      where: { id: id }
+      where: {id: req.params.id},
     })
-      .then(num => {
+      .then((num) => {
         if (num == 1) {
           res.json({
-            success: true
+            success: true,
           });
         } else {
           res.json({
-            success: false
+            success: false,
           });
         }
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(400).json({
           success: false,
-          error: err.message || "Could not delete User with id=" + id
+          error: err.message || 'Could not delete User with id=' + id,
         });
       });
-  }
-}
+  },
+};
 
-module.exports = userController;
+module.exports = UserController;
