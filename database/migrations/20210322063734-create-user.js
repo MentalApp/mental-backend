fe84f09a1fd94f6d64b6cce7cee8198efbde8f8f
Đặng Quang Vũ <43052859+dangquangvu@users.noreply.github.com>
2021-04-20 20:27:41 +0700

    update api user (#40)
    * update api user
    
    * update return response
    
    Co-authored-by: vu dang <vu.dang@sotatek.com>

--- database/migrations/20210322063734-create-user.js
+++ database/migrations/20210322063734-create-user.js
@@ -1,62 +1,77 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+    await queryInterface.createTable(
+      'Users',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        fullName: {
+          type: Sequelize.STRING,
+        },
+        nameWithoutUtf8: {
+          type: Sequelize.STRING,
+        },
+        email: {
+          unique: true,
+          type: Sequelize.STRING,
+        },
+        password: {
+          type: Sequelize.STRING,
+        },
+        militaryCode: {
+          type: Sequelize.STRING,
+        },
+        sex: {
+          type: Sequelize.TINYINT,
+        },
+        isBlock: {
+          type: Sequelize.TINYINT,
+        },
+        address: {
+          type: Sequelize.STRING,
+        },
+        phone: {
+          unique: true,
+          type: Sequelize.STRING,
+        },
+        type: {
+          type: Sequelize.STRING,
+        },
+        role: {
+          type: Sequelize.STRING,
+        },
+        joinArmy: {
+          type: Sequelize.DATE,
+        },
+        unit: {
+          type: Sequelize.INTEGER,
+        },
+        rank: {
+          type: Sequelize.STRING,
+        },
+        position: {
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      fullName: {
-        type: Sequelize.STRING
+      {
+        charset: 'utf8',
       },
-      email: {
-        unique: true,
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      address: {
-        type: Sequelize.STRING
-      },
-      phone: {
-        unique: true,
-        type: Sequelize.STRING
-      },
-      type: {
-        type: Sequelize.STRING
-      },
-      role: {
-        type: Sequelize.INTEGER
-      },
-      joinArmy: {
-        type: Sequelize.DATE
-      },
-      unit: {
-        type: Sequelize.INTEGER
-      },
-      rank: {
-        type: Sequelize.STRING
-      },
-      position: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    },
-    {
-      charset: 'utf8'
-    });
+    );
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Users');
-  }
+  },
 };
