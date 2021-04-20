1b9af79e7cef1362ddf85a298315865475f8b436
namdongngac <ntnam7399@gmail.com>
2021-04-20 22:27:37 +0700

    create predict

---
+++ database/models/predict.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Predict extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  predict.init({
+    officerTestId: DataTypes.INTEGER,
+    predict: DataTypes.INTEGER,
+    conflict: DataTypes.STRING
+  }, {
+    sequelize,
+    charset: 'utf8',
+    modelName: 'Predict',
+  });
+  return Predict;
+};
